c9109fc1217871ef64472e24b7b9977d5dd1e42e
TerryL99 <89864477+TerryL99@users.noreply.github.com>
2021-11-17 16:48:30 -0500

    Add files via upload
    upload monsters & factory, update LOVBoard

---
+++ src/MonsterFactory.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class MonsterFactory {
+    // for creating monsters
+    static final int DRAGON = 1;
+    static final int EXOSKELETON = 2;
+    static final int SPIRIT = 3;
+    static final int BACK = 0;
+    static final String DRAGON_MENU = Path.DRAGON.toString();
+    static final String EXOSKELETON_MENU = Path.EXOSKELETON.toString();
+    static final String SPIRIT_MENU = Path.SPIRIT.toString();
+
+    public MonsterFactory() {}
+
+    public Monster getMonster(int type, int id) {
+        Monster newMonster = selectMonster(type, id);
+        return newMonster;
+    }
+
+
+    private Monster selectMonster(int type, int id) {
+        Monster newMonster;
+        switch (type) {
+            case DRAGON:
+                ArrayList<ArrayList<String>> dragonList = AskInput.read(DRAGON_MENU);
+                if (id == 0) {
+                    return null;
+                }
+                newMonster = getDragon(dragonList.get(id-1));
+                break;
+            case EXOSKELETON:
+                ArrayList<ArrayList<String>> exoskeletonList = AskInput.read(EXOSKELETON_MENU);
+                if (id == 0) {
+                    return null;
+                }
+                newMonster = getExoskeleton(exoskeletonList.get(id-1));
+                break;
+            case SPIRIT:
+                ArrayList<ArrayList<String>> spiritList = AskInput.read(SPIRIT_MENU);
+                if (id == 0) {
+                    return null;
+                }
+                newMonster = getSpirit(spiritList.get(id-1));
+                break;
+            default:
+                return null;
+        }
+        return newMonster;
+    }
+
+    private Dragon getDragon(ArrayList<String> attrs) {
+        Dragon dragon = new Dragon();
+        dragon.setAttributes(attrs);
+
+        return dragon;
+    }
+
+    private Exoskeleton getExoskeleton(ArrayList<String> attrs) {
+        Exoskeleton exoskeleton = new Exoskeleton();
+        exoskeleton.setAttributes(attrs);
+
+        return exoskeleton;
+    }
+
+    private Spirit getSpirit(ArrayList<String> attrs) {
+        Spirit spirit = new Spirit();
+        spirit.setAttributes(attrs);
+
+        return spirit;
+    }
+
+    private void printMenu(ArrayList<ArrayList<String>> monsterList) {
+        String menu;
+        for (int i = 0; i < monsterList.size(); i++) {
+            System.out.print((i + 1) + ": ");
+            for (String s : monsterList.get(i)) {
+                System.out.print(s + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
